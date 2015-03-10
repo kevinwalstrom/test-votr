92cc0de62aa7998e0aac0e5e50d88f82e93a3dc7
kevinwalstrom <noreply>
2015-03-10 18:36:22 +0000

    Add component people

---
+++ ui/people.reel/people.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/people.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class People
+ * @extends Component
+ */
+exports.People = Component.specialize(/** @lends People# */ {
+    constructor: {
+        value: function People() {
+            this.super();
+        }
+    }
+});
