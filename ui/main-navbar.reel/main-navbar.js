f6fce74bb4efab0535dd40b5c20c1e2dcb34231b
kevinwalstrom <krwalstrom@gmail.com>
2015-03-15 22:11:49 -0600

    navbar

---
+++ ui/main-navbar.reel/main-navbar.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/navbar.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Navbar
+ * @extends Component
+ */
+exports.MainNavbar = Component.specialize(/** @lends Navbar# */ {
+    constructor: {
+        value: function MainNavbar() {
+            this.super();
+        }
+    }
+});
